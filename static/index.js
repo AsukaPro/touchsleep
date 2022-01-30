349dacb4c4b39cd9b1b1173ae832c51386a044eb
IkariAsuka <73029455+AsukaPro@users.noreply.github.com>
2022-01-31 04:43:07 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -348,10 +348,10 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
+        if (score <= 49) return '好好学着做人吧';
+        if (score <= 129) return '手速都没老太太快';
+        if (score <= 149) return '咩';
+        if (score <= 199) return '您是老太太还是羊本尊？';
         return '人？';
     }
 
